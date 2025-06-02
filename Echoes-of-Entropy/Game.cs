f58dd671e057f37d7e61a9ae5fb04c4ed04e8527
JL <115737916+yikesboy@users.noreply.github.com>
2025-06-02 21:56:41 +0200

    feat: added keys and fps counter

--- Echoes-of-Entropy/Game.cs
+++ Echoes-of-Entropy/Game.cs
@@ -3,14 +3,26 @@ using Echoes_of_Entropy.Entities;
 using Raylib_cs;
 
 namespace Echoes_of_Entropy;
+
 class Game
 {
     private readonly List<IGameEntity> _entities = new();
+
     public Game()
     {
-        var player = new Player(new Vector2(200, 200));
+        var player = new Player(new Vector2(200, 200),
+            "../../../Assets/image.png");
         _entities.Add(player);
+        var wKey = new Key(new Vector2(500, 450), "W", KeyboardKey.W);
+        _entities.Add(wKey);
+        var sKey = new Key(new Vector2(500, 510), "S", KeyboardKey.S);
+        _entities.Add(sKey);
+        var aKey = new Key(new Vector2(440, 510), "A", KeyboardKey.A);
+        _entities.Add(aKey);
+        var dKey = new Key(new Vector2(560, 510), "D", KeyboardKey.D);
+        _entities.Add(dKey);
     }
+
     static void Main()
     {
         var game = new Game();
@@ -21,16 +33,21 @@ class Game
     private void Setup()
     {
         const String gameName = "Echoes of Entropy";
-        const int screen = 1;
-        int screenHeight = Raylib.GetMonitorHeight(screen);
+        const int screen = 0;
+        var screenHeight = Raylib.GetMonitorHeight(screen);
         Console.WriteLine(screenHeight);
-        int screenWidth = Raylib.GetMonitorWidth(screen);
+        var screenWidth = Raylib.GetMonitorWidth(screen);
         Console.WriteLine(screenWidth);
-        int screenRefreshRate = Raylib.GetMonitorRefreshRate(screen);
+        var screenRefreshRate = Raylib.GetMonitorRefreshRate(screen);
         Console.WriteLine(screenRefreshRate);
-        
+
         Raylib.InitWindow(800, 600, gameName);
-        Raylib.SetTargetFPS(screenRefreshRate);
+        Raylib.SetTargetFPS(165);
+
+        foreach (var entity in _entities)
+        {
+            entity.SetUp();
+        }
     }
 
     private void Loop()
@@ -38,15 +55,16 @@ class Game
         while (!Raylib.WindowShouldClose())
         {
             Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.DarkGray);
+            Raylib.ClearBackground(Color.SkyBlue);
             Raylib.DrawText("Echoes of Entropy.", 20, 20, 20, Color.RayWhite);
 
             foreach (var entity in _entities)
             {
-               entity.Update(); 
-               entity.Draw(); 
+                entity.Update();
+                entity.Draw();
             }
             
+            Raylib.DrawFPS(100, 500);
             Raylib.EndDrawing();
         }
 
