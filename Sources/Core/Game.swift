2349f11e65f7c8ed263e939cce1099ec95743194
JL <115737916+yikesboy@users.noreply.github.com>
2025-06-09 19:16:58 +0200

    refactor: updated game class with abstractions

--- Sources/Core/Game.swift
+++ Sources/Core/Game.swift
@@ -1,11 +1,18 @@
 import Raylib
 
 final class Game {
+    private var sceneManager: SceneManager
     private let inputManager: InputSystem
     private var inputState: InputState
+    private let updateSystem: UpdateSystem
+    private let renderSystem: RenderSystem
 
     init() {
+        self.sceneManager = SceneManager()
         self.inputManager = InputManager()
+        self.inputState = InputState()
+        self.updateSystem = UpdateSystem()
+        self.renderSystem = RenderSystem()
     }
 
     func run() {
@@ -17,18 +24,31 @@ final class Game {
         let gameName: String = "Echoes of Entropy"
         let screenWidth: Int32 = 800
         let screenHeight: Int32 = 450
-        let targetFPS: Int32 = 165
+
         Raylib.initWindow(screenWidth, screenHeight, gameName)
-        //Raylib.setTargetFPS(targetFPS)
+        var scene = GameState()
+
+        let bg = SolidBackground(color: .blue)
+        scene.entities.append(bg)
+        for (index, action) in GameAction.allCases.enumerated() {
+            let position = Vector2(x: Float(index * 100), y: 100.0)
+            let key = KeyOverlay(
+                position: position, label: inputManager.getKey(for: action)?.description ?? "",
+                gameAction: action)
+            scene.entities.append(key)
+        }
+
+        sceneManager.loadScene(scene)
     }
 
     private func loop() {
         while Raylib.windowShouldClose == false {
-            inputManager.updateState(inputState)
-            Raylib.beginDrawing()
-            Raylib.clearBackground(.skyBlue)
-            Raylib.drawFPS(100, 100)
-            Raylib.endDrawing()
+            inputManager.updateState(&inputState)
+            if var scene = sceneManager.currentScene {
+                updateSystem.update(&scene, inputState: inputState)
+                sceneManager.loadScene(scene)
+            }
+            renderSystem.render(sceneManager.currentScene)
         }
 
         Raylib.closeWindow()
