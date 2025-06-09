05e16b33e4d96fc0308e83dcf439da2ac1acb99e
JL <115737916+yikesboy@users.noreply.github.com>
2025-06-09 15:51:10 +0200

    feat: created KeyOverlay ui entitiy

---
+++ Sources/Entities/KeyOverlay.swift
@@ -0,0 +1,27 @@
+import Raylib
+
+struct KeyOverlay: Entity {
+
+    private var position: Vector2
+    private var size: Vector2
+    private var label: String
+    private var isActive: Bool
+    private let gameAction: GameAction
+
+    init(position: Vector2, label: String, gameAction: GameAction) {
+        self.position = position
+        self.size = Vector2(x: 50, y: 50)
+        self.label = label
+        self.isActive = false
+        self.gameAction = gameAction
+    }
+
+    mutating func update(inputState: InputState) {
+        isActive = inputState.isActionDown(gameAction)
+    }
+
+    func draw() {
+        Raylib.drawRectangleV(position, size, isActive ? .white : .gray)
+        Raylib.drawText(label, Int32(position.x), Int32(position.y), 28, .black)
+    }
+}
