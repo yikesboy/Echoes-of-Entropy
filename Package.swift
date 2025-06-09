6b4c89da7c8aa8eb5a2a904a9b9a0c801264e988
JL <115737916+yikesboy@users.noreply.github.com>
2025-06-09 12:30:39 +0200

    build: added raylib dependency

--- Package.swift
+++ Package.swift
@@ -5,10 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "swift-eoe",
+    dependencies: [
+        .package(url: "https://github.com/STREGAsGate/Raylib.git", branch: "master")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "swift-eoe"),
+            name: "swift-eoe")
     ]
 )
