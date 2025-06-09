4deb804ece15fc807be5bdb4360b02c84f843575
JL <115737916+yikesboy@users.noreply.github.com>
2025-06-09 13:35:21 +0200

    feat: created basic game setup and loop

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,30 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Raylib
 
-print("Hello, world!")
+@main
+final class Game {
+    static func main() {
+        let game = Game()
+        game.setup()
+        game.loop()
+    }
+
+    private func setup() {
+        let gameName: String = "Echoes of Entropy"
+        let screenWidth: Int32 = 800
+        let screenHeight: Int32 = 450
+        let targetFPS: Int32 = 165
+        Raylib.initWindow(screenWidth, screenHeight, gameName)
+        Raylib.setTargetFPS(targetFPS)
+    }
+
+    private func loop() {
+        while Raylib.windowShouldClose == false {
+            Raylib.beginDrawing()
+            Raylib.clearBackground(.skyBlue)
+            Raylib.drawFPS(100, 100)
+            Raylib.endDrawing()
+        }
+
+        Raylib.closeWindow()
+    }
+}
