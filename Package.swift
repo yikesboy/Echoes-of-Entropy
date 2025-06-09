55812b58d9a78dc6e569b19bac7eb1e75d9f96ab
JL <115737916+yikesboy@users.noreply.github.com>
2025-06-09 13:34:53 +0200

    build: downloaded raylib dep and made is accesible

--- Package.swift
+++ Package.swift
@@ -12,6 +12,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "swift-eoe")
+            name: "swift-eoe",
+            dependencies: ["Raylib"]
+        )
     ]
 )
