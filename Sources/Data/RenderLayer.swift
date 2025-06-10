a613c16e5b25c5567f22e3d94728f0b4053c9c4e
JL <115737916+yikesboy@users.noreply.github.com>
2025-06-10 15:39:01 +0200

    refactor: added and updated render layers

--- Sources/Data/RenderLayer.swift
+++ Sources/Data/RenderLayer.swift
@@ -1,8 +1,9 @@
 enum RenderLayer: Int, CaseIterable, Comparable {
     case base = 0
-    case decoration = 100
-    case characters = 200
-    case ui = 300
+    case tileMap = 100
+    case decoration = 201
+    case characters = 300
+    case ui = 400
 
     static func < (lhs: RenderLayer, rhs: RenderLayer) -> Bool {
         return lhs.rawValue < rhs.rawValue
